6dea1eeeb99bc3a9b1cb5f5e551bba2b77601e94
KandoSVK <kubo.kanitra@gmail.com>
2015-03-02 18:01:46 +0100

    Set example data for results page

--- routes/results.js
+++ routes/results.js
@@ -7,6 +7,21 @@ var router = express.Router();
 router.get('/',auth.roleAuthenticate(['client']), function(req, res) {
   res.render('results', {
     user: req.user,
+    samples: [
+      {id: 123,
+       patientName: 'Kubko',
+       additionalInfo: 'bLABLA',
+       isDone: false,
+       results: [
+         {
+           id: 1,
+           name: 'GENE-321',
+           description: 'Breast cancer',
+           result: false
+         }
+       ]
+      }
+    ],
     sampleId: 132,
     patientName: 'Kubko',
     additionalInfo: 'Blablabla'
