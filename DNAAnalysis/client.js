276c2e750df9a0e8a2065b642a86e136b605736c
KandoSVK <kubo.kanitra@gmail.com>
2015-04-29 00:48:40 +0200

    Default backbone for client handlers

--- DNAAnalysis/client.js
+++ DNAAnalysis/client.js
@@ -1,9 +1,20 @@
 var JDSM = require('../JDSM/client')('http://localhost:3000');
 module.exports = (function() {
-  console.log('registering task');
-  JDSM.registerTask('pong', function(data, respond) {
-    console.log('Data from server', data);
-    console.log('Responding to server...');
-    respond.respond('pipik');
+  console.log('registering tasks');
+
+  JDSM.registerTask('analyze', function(data, respond){
+
+  })
+
+  JDSM.registerTask('analyzeNoCache', function(data, respond){
+
+  })
+
+  JDSM.registerTask('addClusters', function(data, respond){
+
+  })
+
+  JDSM.registerTask('freeClusters', function(data, respond){
+
   })
 })()
