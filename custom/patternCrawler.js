8cd48f9787a29f02c2356c6e34395648b0c6f44b
KandoSVK <kubo.kanitra@gmail.com>
2015-05-07 13:02:34 +0200

    Added timelimit 15req/sec to fetchEnsemblData gruntTask

--- custom/patternCrawler.js
+++ custom/patternCrawler.js
@@ -8,6 +8,7 @@ var http = require('http');
 var util = require('util');
 var models = require('../models');
 var mysql = require('mysql');
+var colors = require('colors');
 module.exports = {
 
   /**
@@ -40,7 +41,9 @@ module.exports = {
       sequenceEnd: data.sequenceEnd
     }).then(function(pattern){
       callback();
-    });
+    }).catch(function(err){
+      console.log(err);
+    })
   },
 
   /**
@@ -71,42 +74,53 @@ module.exports = {
       if (err) {
         throw err;
       }
-
+      var rowsLeft = rows.length;
+      var counter = 0;
       for (var i in rows) {
-        var path = 'http://rest.ensembl.org/sequence/id/'+rows[i].stable_id+'?content-type=application/json';
-        temporaryObjects[rows[i].stable_id] = {
-          name: rows[i].stable_id,
-          description: rows[i].description,
-          isForwardStrand: rows[i].seq_region_strand == 1
-        };
+        counter++;
+        setTimeout(function(rowObj){
+          console.log('Calling function'.red, Date.now());
+          //ensembl allows only 15 requests per second -> need to put sleep
+          var path = 'http://rest.ensembl.org/sequence/id/'+rowObj.stable_id+'?content-type=application/json';
+          temporaryObjects[rowObj.stable_id] = {
+            name: rowObj.stable_id,
+            description: rowObj.description,
+            isForwardStrand: rowObj.seq_region_strand == 1
+          };
 
-        http.get(path, function(result){
-           var data = '';
-           result.on('data', function(chunk){
-             data += chunk;
-           });
-           result.on('end', function(){
-             data = JSON.parse(data);
-             temporaryObjects[data.id]['data'] = data.seq;
-             //parse from data.desc, chromosome number and position from:to
-             //e.q chromosome:GRCh38:CHR_HSCHR6_MHC_COX_CTG1:29552212:29556525:-1
-             var parts = data.desc.split(':');
-             temporaryObjects[data.id]['sequenceStart'] = parts[parts.length - 3];
-             temporaryObjects[data.id]['sequenceEnd'] = parts[parts.length - 2];
-             var chromosome = parts[parts.length - 4];
-             chromosome = chromosome.substring(chromosome.length - 2, chromosome.length);
-             temporaryObjects[data.id]['chromosome'] = isNaN(parseInt(chromosome))?parseInt(chromosome[1]):parseInt(chromosome);
-             //save object
-             self.savePattern(temporaryObjects[data.id],function(){
-               delete temporaryObjects[data.id];
-               if (Object.keys(temporaryObjects).length == 0)
-                 callback(null, true);
-             });
-           });
-        }).on('error', function(e){
-           console.log('REQ ERROR: '+ e.message);
-           callback(true, null);
-        });
+          http.get(path, function(result){
+            var data = '';
+            result.on('data', function(chunk){
+              data += chunk;
+            });
+            result.on('end', function(){
+              try {
+                data = JSON.parse(data);
+              }catch(err) {
+                console.log(data);
+                throw err;
+              }
+              temporaryObjects[data.id]['data'] = data.seq;
+              //parse from data.desc, chromosome number and position from:to
+              //e.q chromosome:GRCh38:CHR_HSCHR6_MHC_COX_CTG1:29552212:29556525:-1
+              var parts = data.desc.split(':');
+              temporaryObjects[data.id]['sequenceStart'] = parts[parts.length - 3];
+              temporaryObjects[data.id]['sequenceEnd'] = parts[parts.length - 2];
+              var chromosome = parts[parts.length - 4];
+              chromosome = chromosome.substring(chromosome.length - 2, chromosome.length);
+              temporaryObjects[data.id]['chromosome'] = isNaN(parseInt(chromosome))?parseInt(chromosome[1]):parseInt(chromosome);
+              //save object
+              self.savePattern(temporaryObjects[data.id],function(){
+                if (--rowsLeft == 0)
+                  callback(null, true);
+              });
+            });
+          }).on('error', function(e){
+            console.log('REQ ERROR: '+ e.message);
+            callback(true, null);
+          });
+        }, counter*(1000/14), rows[i]);
+
       }
 
 
